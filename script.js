2c31a486cff1a6ef1c4d0d0ebe180476e2daac7a
Adventure By PEAK-LK <148787340+adventurebyPEAK-LK@users.noreply.github.com>
2026-07-27 02:22:36 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const gear = [
     {
       name: "4 Person Tent (Automatic / Manual)",
 
-      image: "https://images.unsplash.com/photo-1785079824476-b508183253fc?q=80&w=1937&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      image: "https://images.unsplash.com/photo-1785094221698-3ae8ab84d658?q=80&w=1115&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 
       rentPrice: {
         manual: 1000,
@@ -28,7 +28,7 @@ const gear = [
     {
       name: "5 Person Tent (Manual)",
 
-      image: "https://images.unsplash.com/photo-1785079911285-1aa3e1f18652?q=80&w=972&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      image: "https://images.unsplash.com/photo-1785094221700-c0606e7d3db4?q=80&w=994&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 
       rentPrice: 1300,
       buyPrice: 32000
@@ -37,7 +37,7 @@ const gear = [
     {
       name: "6 Person Tent (Automatic)",
 
-      image: "https://images.unsplash.com/photo-1785079996863-d36275ee2128?q=80&w=992&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      image: "https://images.unsplash.com/photo-1785094221730-ac1a31764aa6?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 
       rentPrice: 1500,
       buyPrice: 38000
@@ -46,7 +46,7 @@ const gear = [
     {
       name: "Decathlon Quechua 2-3 Person",
 
-      image: "https://images.unsplash.com/photo-1785080075354-5b5ee6151023?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      image: "https://images.unsplash.com/photo-1785094221869-63a76da9c367?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 
       rentPrice: 1500,
       buyPrice: 42900
@@ -55,7 +55,7 @@ const gear = [
     {
       name: "Decathlon Quechua 3-4 Person",
 
-      image: "images/tents/decathlon-3-4.jpg",
+      image: "https://images.unsplash.com/photo-1785094222048-b2b666220350?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 
       rentPrice: 2400,
       buyPrice: 55000
@@ -64,7 +64,7 @@ const gear = [
     {
       name: "Coleman 6-7 Person",
 
-      image: "images/tents/coleman-6-7.jpg",
+      image: "https://images.unsplash.com/photo-1785094221829-f6f85ce569a8?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 
       rentPrice: 1800,
       buyPrice: 22300
@@ -82,11 +82,13 @@ image:"https://images.unsplash.com/photo-1784839288582-9ac718e422ab?q=80&w=1170&
 items:[
 {
     name:"Tent Mattress",
+    image:"https://images.unsplash.com/photo-1785094222035-a435de234da5?q=80&w=845&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     rentPrice:300,
     buyPrice:5700
 },
 {
     name:"Sleeping Bag",
+    image:"https://images.unsplash.com/photo-1785094222042-23d06cf98897?q=80&w=869&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     rentPrice:350,
     buyPrice:11500
 }
@@ -104,41 +106,47 @@ image:"https://images.unsplash.com/photo-1784837891663-1073fa2e6774?q=80&w=1170&
 items:[
 {
     name:"BBQ Grill Small (with tongs)",
+    image:"https://images.unsplash.com/photo-1785094221740-550dcccc9f6e?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     rentPrice:500,
     buyPrice:6000
 },
 {
     name:"BBQ Grill Large (with tongs)",
+    image:"https://images.unsplash.com/photo-1785094221765-8d21728253b1?q=80&w=931&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     rentPrice:950,
     buyPrice:11600
 },
 {
     name:"BBQ Charcoal 1kg",
-    rentPrice:250,
-    buyPrice:600
+    image:"https://images.unsplash.com/photo-1785094221729-a86ab81604ad?q=80&w=1172&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    buyPrice:300
 },
 {
     name:"Wind Proof Gas Stove",
+    image:"https://images.unsplash.com/photo-1785094222072-2a809d81494d?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     rentPrice:500,
     buyPrice:4500
 },
 {
     name:"Butane Gas Canister",
-    rentPrice:950,
+    image:"https://images.unsplash.com/photo-1785094221780-bcd6a5f3f4f8?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     buyPrice:1000
 },
 {
     name:"Cooking Pot / Kettle / Pan",
+    image:"https://images.unsplash.com/photo-1785094221833-366ba227f7d0?q=80&w=798&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     rentPrice:200,
     buyPrice:5800
 },
 {
     name:"Knife & Cutting Board",
+    image:"https://images.unsplash.com/photo-1785094221913-f52f12d487a9?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     rentPrice:100,
     buyPrice:2200
 },
 {
     name:"Plates / Cups / Cutlery",
+    image:"https://images.unsplash.com/photo-1785094221837-17adf08ebaf1?q=80&w=1124&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     rentPrice:20,
     buyPrice:550
 }
@@ -156,21 +164,25 @@ image:"https://images.unsplash.com/photo-1784838276193-ea10359c3c60?q=80&w=1170&
 items:[
 {
     name:"Rechargeable Lamp",
+    image:"https://images.unsplash.com/photo-1785094222072-dee887314209?q=80&w=496&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     rentPrice:300,
     buyPrice:5200
 },
 {
     name:"Torch",
+    image:"https://images.unsplash.com/photo-1785094222031-2c9ce2477b34?q=80&w=1156&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     rentPrice:300,
     buyPrice:2400
 },
 {
     name:"Head Torch",
+    image:"https://images.unsplash.com/photo-1785094222439-97676647ecd2?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     rentPrice:250,
     buyPrice:1500
 },
 {
     name:"Kerosene Oil Lantern",
+    image:"https://images.unsplash.com/photo-1785094221997-63f891c19d3d?q=80&w=538&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     rentPrice:350,
     buyPrice:3800
 }
@@ -188,21 +200,25 @@ image:"https://images.unsplash.com/photo-1784838276223-d34984becb1f?q=80&w=1170&
 items:[
 {
     name:"Camping Chair Large",
+    image:"https://images.unsplash.com/photo-1785094221820-c97f49c3fc71?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     rentPrice:400,
     buyPrice:9500
 },
 {
     name:"Camping Chair Small",
+    image:"https://images.unsplash.com/photo-1785094221786-2313fe98a272?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     rentPrice:300,
     buyPrice:4200
 },
 {
     name:"Foldable Stool",
+    image:"https://images.unsplash.com/photo-1785094221964-890f382ae974?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     rentPrice:250,
     buyPrice:3700
 },
 {
     name:"Foldable Table",
+    image:"https://images.unsplash.com/photo-1785094221964-b0fe1f913bea?q=80&w=945&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     rentPrice:500,
     buyPrice:11200
 }
@@ -220,18 +236,32 @@ image:"https://images.unsplash.com/photo-1784838276177-327a16fc5443?q=80&w=2070&
 items:[
 {
     name:"Backpack 60L",
+    image:"https://images.unsplash.com/photo-1785094221708-9c84c46c4e0e?q=80&w=632&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     rentPrice:450,
     buyPrice:7200
 },
 {
+    name:"Hiking Stick",
+    image:"https://images.unsplash.com/photo-1785094222455-194e9529be47?q=80&w=510&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    rentPrice:500,
+    buyPrice:2000
+},
+{
     name:"Leech Socks",
+    image:"https://images.unsplash.com/photo-1785094222048-359df8bece27?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     rentPrice:250,
     buyPrice:1000
 },
 {
     name:"Leech Repellent Spray",
-    rentPrice:320,
-    buyPrice:800
+    image:"https://images.unsplash.com/photo-1785094221991-604e28547105?q=80&w=640&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+
+    buyPrice: {
+        "30ml": 300,
+        "50ml": 500
+    },
+
+    buyType: "size"
 }
 ]
 
@@ -246,26 +276,36 @@ image:"https://images.unsplash.com/photo-1784838276169-7401d6381a9b?q=80&w=1170&
 items:[
 {
     name:"Hammock",
+    image:"https://images.unsplash.com/photo-1785094221989-60b189e03b94?q=80&w=1962&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     rentPrice:400,
     buyPrice:7200
 },
 {
     name:"Bluetooth Speaker",
+    image:"https://images.unsplash.com/photo-1785094221764-dacc7fda132c?q=80&w=631&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     rentPrice:250,
     buyPrice:4000
 },
 {
     name:"Bongo Drum",
+    image:"https://images.unsplash.com/photo-1785094221801-447217000ea7?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     rentPrice:300,
     buyPrice:12700
 },
 {
-    name:"Power Bank",
-    rentPrice:850,
-    buyPrice:9000
+    name:"Power Bank (10000mAh)",
+    image:"https://images.unsplash.com/photo-1785094222001-75735983966b?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    buyPrice:4000
+},
+
+{
+    name:"Power Bank (20000mAh)",
+    image:"https://images.unsplash.com/photo-1785094222013-dafc6516f7b3?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    buyPrice:7000
 },
 {
     name:"Gas Torch / Flame Gun",
+    image:"https://images.unsplash.com/photo-1785094221977-7e9604ac5c66?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     rentPrice:500,
     buyPrice:3800
 }
@@ -357,6 +397,8 @@ renderGear();
 
 // OPEN CATEGORY ITEMS
 
+// OPEN CATEGORY ITEMS
+
 function openCategory(name){
 
     let category = gear.find(x => x.name === name);
@@ -386,40 +428,83 @@ function openCategory(name){
                 <div class="price">
 
                 ${
-                    typeof item.rentPrice === "object"
-                    ? `
-                    <div><b>Rent (Manual):</b> Rs. ${item.rentPrice.manual.toLocaleString()} <small>/ DAY</small></div>
-                    <div><b>Rent (Automatic):</b> Rs. ${item.rentPrice.automatic.toLocaleString()} <small>/ DAY</small></div>
-                    `
-                    : `
-                    <div><b>Rent:</b> Rs. ${item.rentPrice.toLocaleString()} <small>/ DAY</small></div>
-                    `
+                    item.rentPrice
+                    ? typeof item.rentPrice === "object"
+                        ? `
+                        <div>
+                            <b>Rent (Manual):</b>
+                            Rs. ${item.rentPrice.manual.toLocaleString()}
+                            <small>/ DAY</small>
+                        </div>
+
+                        <div>
+                            <b>Rent (Automatic):</b>
+                            Rs. ${item.rentPrice.automatic.toLocaleString()}
+                            <small>/ DAY</small>
+                        </div>
+                        `
+                        : `
+                        <div>
+                            <b>Rent:</b>
+                            Rs. ${item.rentPrice.toLocaleString()}
+                            <small>/ DAY</small>
+                        </div>
+                        `
+                    : ""
                 }
 
                 ${
-                    typeof item.buyPrice === "object"
-                    ? `
-                    <div><b>Buy (Manual):</b> Rs. ${item.buyPrice.manual.toLocaleString()}</div>
-                    <div><b>Buy (Automatic):</b> Rs. ${item.buyPrice.automatic.toLocaleString()}</div>
-                    `
-                    : `
-                    <div><b>Buy:</b> Rs. ${item.buyPrice.toLocaleString()}</div>
-                    `
-                }
+    item.buyType === "size"
+    ? `
+    <div>
+        <b>Buy (30ml):</b>
+        Rs. ${item.buyPrice["30ml"].toLocaleString()}
+    </div>
+
+    <div>
+        <b>Buy (50ml):</b>
+        Rs. ${item.buyPrice["50ml"].toLocaleString()}
+    </div>
+    `
+    : typeof item.buyPrice === "object"
+    ? `
+    <div>
+        <b>Buy (Manual):</b>
+        Rs. ${item.buyPrice.manual.toLocaleString()}
+    </div>
+
+    <div>
+        <b>Buy (Automatic):</b>
+        Rs. ${item.buyPrice.automatic.toLocaleString()}
+    </div>
+    `
+    : `
+    <div>
+        <b>Buy:</b>
+        Rs. ${item.buyPrice.toLocaleString()}
+    </div>
+    `
+}
 
                 </div>
 
-                <button class="rent-btn"
-                onclick="openBooking(
-                    '${item.name}',
-                    '${
-                        typeof item.rentPrice === "object"
-                        ? "Rs. " + item.rentPrice.manual
-                        : "Rs. " + item.rentPrice
-                    }'
-                )">
-                    ↗
-                </button>
+                ${
+                    item.rentPrice
+                    ? `
+                    <button class="rent-btn"
+                    onclick="openBooking(
+                        '${item.name}',
+                        '${
+                            typeof item.rentPrice === "object"
+                            ? "Rs. " + item.rentPrice.manual
+                            : "Rs. " + item.rentPrice
+                        }'
+                    )">
+                        ↗
+                    </button>
+                    `
+                    : ""
+                }
 
             </div>
 
